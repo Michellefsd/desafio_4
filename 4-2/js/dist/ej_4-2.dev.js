0cc3d2939264e93b850e248bd00967bc916d78f4
Michellefsd <promichfsd@gmail.com>
2023-08-04 18:19:31 -0300

    documento csv generado en javascript

--- 4-2/js/dist/ej_4-2.dev.js
+++ 4-2/js/dist/ej_4-2.dev.js
@@ -1,9 +1,10 @@
 "use strict";
 
-var key = "10971b499be61b351760e8d57f0e3a4d"; //Funcion//
+var key = "10971b499be61b351760e8d57f0e3a4d";
+var capitalesLatam = ["buenos aires", "la paz", "brasilia", "santiago de chile", "san jose,cr", "bogota", "habana", "quito", "san salvador", "guatemala", "Port-au-Prince", "tegucigalpa", "mexico,mx", "managua", "panama", "asuncion", "lima", "Santo Domingo", "montevideo", "caracas"]; //Funcion//
 
 var obtener_datos_climaticos = function obtener_datos_climaticos(capital) {
-  var capitales, _iteratorNormalCompletion, _didIteratorError, _iteratorError, _iterator, _step, ciudad, response;
+  var capitales, datos_climaticos_capitales, _iteratorNormalCompletion, _didIteratorError, _iteratorError, _iterator, _step, ciudad, response, csvContent, headers, encodedUri, link;
 
   return regeneratorRuntime.async(function obtener_datos_climaticos$(_context) {
     while (1) {
@@ -11,20 +12,21 @@ var obtener_datos_climaticos = function obtener_datos_climaticos(capital) {
         case 0:
           /////////     // pedazo de codigo generado por chatGPT///////////////////////
           capitales = capital ? [capital] : capitalesLatam;
+          datos_climaticos_capitales = [];
           _iteratorNormalCompletion = true;
           _didIteratorError = false;
           _iteratorError = undefined;
-          _context.prev = 4;
+          _context.prev = 5;
           _iterator = capitales[Symbol.iterator]();
 
-        case 6:
+        case 7:
           if (_iteratorNormalCompletion = (_step = _iterator.next()).done) {
-            _context.next = 14;
+            _context.next = 15;
             break;
           }
 
           ciudad = _step.value;
-          _context.next = 10;
+          _context.next = 11;
           return regeneratorRuntime.awrap(fetch("http://api.openweathermap.org/data/2.5/weather?q=".concat(ciudad, "&appid=").concat(key, "&units=metric&lang=es")).then(function (response) {
             return response.json();
           }).then(function (data) {
@@ -38,57 +40,86 @@ var obtener_datos_climaticos = function obtener_datos_climaticos(capital) {
             var max = data['main']['temp_max'];
             var min = data['main']['temp_min'];
             console.log("Ciudad: " + capital + ", país: " + pais + ", descripción del clima: " + descripcion + ", sensación térmica: " + sensacion + "°, presión: " + presion + "hPa, humedad: " + humedad + "%, temperatura mínima: " + min + "°, temperatura máxima: " + max + "°.");
+            datos_climaticos_capitales.push({
+              "Capital": capital,
+              "Pais": pais,
+              "Descripcion": descripcion,
+              "Sensacion": sensacion,
+              "Presion": presion,
+              "Humedad": humedad,
+              "Max": max,
+              "Min": min
+            });
           }));
 
-        case 10:
+        case 11:
           response = _context.sent;
 
-        case 11:
+        case 12:
           _iteratorNormalCompletion = true;
-          _context.next = 6;
+          _context.next = 7;
           break;
 
-        case 14:
-          _context.next = 20;
+        case 15:
+          _context.next = 21;
           break;
 
-        case 16:
-          _context.prev = 16;
-          _context.t0 = _context["catch"](4);
+        case 17:
+          _context.prev = 17;
+          _context.t0 = _context["catch"](5);
           _didIteratorError = true;
           _iteratorError = _context.t0;
 
-        case 20:
-          _context.prev = 20;
+        case 21:
           _context.prev = 21;
+          _context.prev = 22;
 
           if (!_iteratorNormalCompletion && _iterator["return"] != null) {
             _iterator["return"]();
           }
 
-        case 23:
-          _context.prev = 23;
+        case 24:
+          _context.prev = 24;
 
           if (!_didIteratorError) {
-            _context.next = 26;
+            _context.next = 27;
             break;
           }
 
           throw _iteratorError;
 
-        case 26:
-          return _context.finish(23);
-
         case 27:
-          return _context.finish(20);
+          return _context.finish(24);
 
         case 28:
+          return _context.finish(21);
+
+        case 29:
+          // Pedazo de código generado por chatGPT
+          // Genero el contenido  de CSV 
+          csvContent = "data:text/csv;charset=utf-8,";
+          headers = ["Capital", "Pais", "Descripcion", "Sensacion", "Presion", "Humedad", "Max", "Min"];
+          csvContent += headers.join(";") + "\n";
+          datos_climaticos_capitales.forEach(function (datos) {
+            var values = headers.map(function (header) {
+              return datos[header];
+            });
+            csvContent += values.join(";") + "\n";
+          }); // creo un link y genero la descarga
+
+          encodedUri = encodeURI(csvContent);
+          link = document.createElement("a");
+          link.setAttribute("href", encodedUri);
+          link.setAttribute("download", "datos_climaticos_latam.csv");
+          document.body.appendChild(link); // Required for Firefox
+
+          link.click(); // fin del código generado por ChatGPT
+
+        case 39:
         case "end":
           return _context.stop();
       }
     }
-  }, null, null, [[4, 16, 20, 28], [21,, 23, 27]]);
+  }, null, null, [[5, 17, 21, 29], [22,, 24, 28]]);
 };
-
-var capitalesLatam = ["buenos aires", "la paz", "brasilia", "santiago de chile", "san jose,cr", "bogota", "habana", "quito", "san salvador", "guatemala", "Port-au-Prince", "tegucigalpa", "mexico,mx", "managua", "panama", "asuncion", "lima", "Santo Domingo", "montevideo", "caracas"];
 //# sourceMappingURL=ej_4-2.dev.js.map
--- 4-2/js/ej_4-2.js
+++ 4-2/js/ej_4-2.js
@@ -1,30 +1,4 @@
-
-
 const key = "10971b499be61b351760e8d57f0e3a4d"
-//Funcion//
-const obtener_datos_climaticos = async(capital) => {
-/////////     // pedazo de codigo generado por chatGPT///////////////////////
-    const capitales = capital ? [capital] : capitalesLatam
-    for (const ciudad of capitales) {
-/////////////////////////fin del codigo generado chatGPT///////////////////////
-
-        const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${key}&units=metric&lang=es`)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            const capital = data['name'];
-            const pais = data['sys']['country'];
-            const descripcion = data['weather'][0]['description'];
-            const sensacion = data['main']['feels_like'];
-            const presion = data['main']['pressure'];
-            const humedad = data['main']['humidity'];
-            const max = data['main']['temp_max'];
-            const min = data['main']['temp_min'];
-            console.log("Ciudad: " + capital + ", país: " + pais + ", descripción del clima: " + descripcion + ", sensación térmica: " + sensacion + "°, presión: " + presion + "hPa, humedad: " + humedad + "%, temperatura mínima: " + min + "°, temperatura máxima: " + max + "°.");
-        })
-    }
-}
-
 const capitalesLatam = [
     "buenos aires", 
     "la paz", 
@@ -47,4 +21,60 @@ const capitalesLatam = [
     "montevideo", 
     "caracas"  
 ];
+
+
+//Funcion//
+const obtener_datos_climaticos = async(capital) => {
+/////////     // pedazo de codigo generado por chatGPT///////////////////////
+    const capitales = capital ? [capital] : capitalesLatam
+    const datos_climaticos_capitales = [];
+    for (const ciudad of capitales) {
+/////////////////////////fin del codigo generado chatGPT///////////////////////
+
+        const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${key}&units=metric&lang=es`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            const capital = data['name'];
+            const pais = data['sys']['country'];
+            const descripcion = data['weather'][0]['description'];
+            const sensacion = data['main']['feels_like'];
+            const presion = data['main']['pressure'];
+            const humedad = data['main']['humidity'];
+            const max = data['main']['temp_max'];
+            const min = data['main']['temp_min'];
+            console.log("Ciudad: " + capital + ", país: " + pais + ", descripción del clima: " + descripcion + ", sensación térmica: " + sensacion + "°, presión: " + presion + "hPa, humedad: " + humedad + "%, temperatura mínima: " + min + "°, temperatura máxima: " + max + "°.");
+            datos_climaticos_capitales.push({
+                "Capital": capital,
+                "Pais": pais,
+                "Descripcion": descripcion,
+                "Sensacion": sensacion,
+                "Presion": presion,
+                "Humedad": humedad,
+                "Max": max,
+                "Min": min,
+            });
+        });
+    }
+// Pedazo de código generado por chatGPT
+    // Genero el contenido  de CSV 
+    let csvContent = "data:text/csv;charset=utf-8,";
+    const headers = ["Capital", "Pais", "Descripcion", "Sensacion", "Presion", "Humedad", "Max", "Min"];
+    csvContent += headers.join(";") + "\n";
+
+    datos_climaticos_capitales.forEach(datos => {
+        const values = headers.map(header => datos[header]);
+        csvContent += values.join(";") + "\n";
+    });
+
+    // creo un link y genero la descarga
+    const encodedUri = encodeURI(csvContent);
+    const link = document.createElement("a");
+    link.setAttribute("href", encodedUri);
+    link.setAttribute("download", "datos_climaticos_latam.csv");
+    document.body.appendChild(link); // Required for Firefox
+    link.click();
+// fin del código generado por ChatGPT
+}
+
 
